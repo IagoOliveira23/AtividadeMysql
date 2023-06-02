97ee22c39e770537ba319cd4cba5eb9cb4316eef
Iago <81774359+IagoOliveira23@users.noreply.github.com>
2023-06-02 16:47:15 -0300

    Update hospital2.sql

--- Hospital/hospital2.sql
+++ Hospital/hospital2.sql
@@ -1,29 +1,9 @@
-CREATE DATABASE  IF NOT EXISTS `hospital2` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `hospital2`
 USE `hospital2`;
--- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: hospital2
--- ------------------------------------------------------
--- Server version	8.0.33
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `consultas`
---
 
 DROP TABLE IF EXISTS `consultas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `consultas` (
   `id` int NOT NULL AUTO_INCREMENT,
   `paciente_id` int NOT NULL,
@@ -36,50 +16,28 @@ CREATE TABLE `consultas` (
   CONSTRAINT `consultas_ibfk_1` FOREIGN KEY (`paciente_id`) REFERENCES `pacientes` (`id`),
   CONSTRAINT `consultas_ibfk_2` FOREIGN KEY (`medico_id`) REFERENCES `medicos` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `consultas`
---
 
 LOCK TABLES `consultas` WRITE;
-/*!40000 ALTER TABLE `consultas` DISABLE KEYS */;
 INSERT INTO `consultas` VALUES (1,1,1,'2023-06-01','10:00:00'),(2,2,2,'2023-06-02','14:30:00'),(3,3,3,'2023-06-03','11:15:00'),(4,3,2,'2023-05-03','10:05:00'),(5,4,4,'2023-06-03','09:00:00'),(6,3,1,'2023-05-02','10:05:00'),(7,3,4,'2023-06-01','10:05:00'),(8,3,1,'2023-05-20','10:05:00');
-/*!40000 ALTER TABLE `consultas` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `medicos`
---
 
 DROP TABLE IF EXISTS `medicos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `medicos` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
   `especialidade` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `medicos`
---
 
 LOCK TABLES `medicos` WRITE;
-/*!40000 ALTER TABLE `medicos` DISABLE KEYS */;
 INSERT INTO `medicos` VALUES (1,'Dr. Carlos Lima','Cardiologia'),(2,'Dra. Ana Oliveira','Pediatria'),(3,'Dr. André Santos','Ortopedia'),(4,'Dra. Luana Truman','Endocrinologista');
-/*!40000 ALTER TABLE `medicos` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `pacientes`
---
 
 DROP TABLE IF EXISTS `pacientes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pacientes` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
@@ -88,25 +46,14 @@ CREATE TABLE `pacientes` (
   `endereco` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `pacientes`
---
 
 LOCK TABLES `pacientes` WRITE;
-/*!40000 ALTER TABLE `pacientes` DISABLE KEYS */;
 INSERT INTO `pacientes` VALUES (1,'João Silva',35,'Masculino','Rua A, 123'),(2,'Maria Santos',42,'Feminino','Av. B, 456'),(3,'Pedro Rocha',28,'Masculino','Rua C, 789'),(4,'Helena Nogueira',19,'Feminino','Rua C, 789');
-/*!40000 ALTER TABLE `pacientes` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `prontuarios`
---
 
 DROP TABLE IF EXISTS `prontuarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `prontuarios` (
   `id` int NOT NULL AUTO_INCREMENT,
   `paciente_id` int NOT NULL,
@@ -121,25 +68,10 @@ CREATE TABLE `prontuarios` (
   CONSTRAINT `prontuarios_ibfk_1` FOREIGN KEY (`paciente_id`) REFERENCES `pacientes` (`id`),
   CONSTRAINT `prontuarios_ibfk_2` FOREIGN KEY (`medico_id`) REFERENCES `medicos` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `prontuarios`
---
+
 
 LOCK TABLES `prontuarios` WRITE;
-/*!40000 ALTER TABLE `prontuarios` DISABLE KEYS */;
 INSERT INTO `prontuarios` VALUES (1,1,1,'2023-06-01','10:00:00','Pressão alta','AAS 100mg diariamente'),(2,2,2,'2023-06-02','14:30:00','Resfriado','Repouso e hidratação'),(3,3,3,'2023-06-03','11:15:00','Fratura no braço','Imobilização e analgésicos'),(4,3,2,'2023-05-03','10:05:00','Enxaqueca','Relaxamento muscular e analgésicos'),(5,4,4,'2023-06-03','09:00:00','hiperandrogenismo','Terapia hormonal'),(6,3,1,'2023-05-03','10:05:00','Pressão alta','BBs 150mg diariamente'),(7,3,4,'2023-06-01','10:05:00','hipertireoidismo','Dieta a base de iodo');
-/*!40000 ALTER TABLE `prontuarios` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-06-02  9:15:08
